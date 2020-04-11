dd6d1dfb0d497cc777c06eec1226498efc12f20d
jarqprog <jaroslaw.kucharczyk@tesco.com>
2020-04-11 17:40:40 +0200

    add logging

--- src/main/kotlin/com/jarqprog/userapi/readuser/ReadUserController.kt
+++ src/main/kotlin/com/jarqprog/userapi/readuser/ReadUserController.kt
@@ -1,14 +1,33 @@
 package com.jarqprog.userapi.readuser
 
-import com.jarqprog.userapi.domain.User
-import org.springframework.web.bind.annotation.*
-import java.lang.RuntimeException
+import com.jarqprog.userapi.domain.UserModel
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/api/user")
 class ReadUserController(private val repository: ReadUserRepository) {
 
+    val log = LoggerFactory.getLogger(ReadUserController::class.simpleName)
+
     @GetMapping("/{login}")
-    fun findOne(@PathVariable login: String): User = repository.findById(login)
-            .orElseThrow{ RuntimeException("User not found by login {$login}") }
+    fun findOne(@PathVariable login: String): ResponseEntity<UserModel> {
+        val optionalUser = repository.findById(login)
+                .map { optionalUser ->
+                    log.info("about to call by User with login: $login")
+                    optionalUser
+                 }
+                .map { optionalUser -> UserModel(optionalUser) }
+                .or {
+                    log.warn("User not found by login: $login")
+                    Optional.empty()
+                }
+
+        return ResponseEntity.of(optionalUser)
+    }
 }
